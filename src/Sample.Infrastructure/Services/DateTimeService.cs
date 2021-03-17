bda9bbaa0282a53738fa63a841e06b2c819a0a6e
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-17 17:56:39 +0300

    Add DateTimeService

---
+++ src/Sample.Infrastructure/Services/DateTimeService.cs
@@ -0,0 +1,16 @@
+﻿using Sample.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Infrastructure.Services
+{
+    public class DateTimeService : IDateTimeService
+    {
+        public DateTime Now => DateTime.Now;
+
+        public DateTime Tomorrow => Now.AddDays(1);
+    }
+}
