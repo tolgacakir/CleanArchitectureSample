6e751dfd4e321136380eb1a95cd5b9453e34bc97
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-17 17:45:05 +0300

    Add IPassivable

---
+++ src/Sample.Domain/Common/IPassivable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Domain.Common
+{
+    public interface IPassivable
+    {
+        bool IsActive { get; set; }
+    }
+}
