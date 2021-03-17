de5ab28fef816b6865817917352b5c537253b255
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-17 17:55:59 +0300

    Add IApplicationDbContext

---
+++ src/Sample.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Product> Products { get; set; }
+    }
+}
