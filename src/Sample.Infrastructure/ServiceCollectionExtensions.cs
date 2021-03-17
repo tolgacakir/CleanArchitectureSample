a0692dbde242c1b3449f2bed331ed8479568a14f
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-17 17:57:09 +0300

    Add DI Extension for Infrastructure Layer

---
+++ src/Sample.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sample.Application.Common.Interfaces;
+using Sample.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfrastructureLayer(this IServiceCollection services)
+        {
+            services.AddTransient<IDateTimeService, DateTimeService>();
+        }
+    }
+}
