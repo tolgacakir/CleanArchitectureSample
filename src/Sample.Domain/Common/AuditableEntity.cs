58492fe63d92ee66ee1a14d3c92626f54c96710b
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-17 17:45:41 +0300

    Add AuditableEntity

---
+++ src/Sample.Domain/Common/AuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Domain.Common
+{
+    public abstract class AuditableEntity<TPrimaryKey> : Entity<TPrimaryKey>
+    {
+        public DateTime Created { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? LastModified { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
