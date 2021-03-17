985f49f2594e0cf28868ed8b0f01be4258639e46
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-17 17:48:47 +0300

    Add IDateTimeService

---
+++ src/Sample.Application/Common/Interfaces/IDateTimeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Application.Common.Interfaces
+{
+    public interface IDateTimeService
+    {
+        DateTime Now { get; }
+        DateTime Tomorrow { get; }
+    }
+}
