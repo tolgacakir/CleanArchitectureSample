1037bea078e80bd168a21e416b8cacc3a2885df0
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-17 17:48:29 +0300

    Add NotFoundException

---
+++ src/Sample.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Application.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() : base()
+        {
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+        }
+
+        public NotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+        }
+    }
+}
