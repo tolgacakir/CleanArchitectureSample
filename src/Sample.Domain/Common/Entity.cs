e812e68ccb3f177dd53a68c00c51bd95313524a8
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-17 17:44:53 +0300

    Add Entity

---
+++ src/Sample.Domain/Common/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Domain.Common
+{
+    public abstract class Entity<TPrimaryKey>
+    {
+        public TPrimaryKey Id { get; set; }
+    }
+}
