659c9452f042014959600b0f75c6a9c34569474d
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-17 17:45:15 +0300

    Add ISoftable

---
+++ src/Sample.Domain/Common/ISoftDelete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Domain.Common
+{
+    public interface ISoftDelete
+    {
+        bool IsDeleted { get; set; }
+    }
+}
