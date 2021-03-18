a6558d6a8e7b4b9e149b97a80a42464a5d45a92c
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-18 11:56:56 +0300

    Add ServiceCollectionExtensions

---
+++ src/Sample.Persistence/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Sample.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sample.Application.Common.Interfaces;
+
+namespace Sample.Persistence
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseInMemoryDatabase("SampleInMemoryDB"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            }
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+        }
+    }
+}
