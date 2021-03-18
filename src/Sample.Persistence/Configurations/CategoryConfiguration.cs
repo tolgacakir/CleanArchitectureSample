a8ef1d5fa41159059b8f1d1cc30c1da4087682ae
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-18 11:55:24 +0300

    Add Entity Configuration

---
+++ src/Sample.Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sample.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Persistence.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(e => e.Id).HasColumnName("ID");
+
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(20);
+        }
+    }
+}
---
+++ src/Sample.Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sample.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Persistence.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(p => p.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(e => e.UnitPrice)
+                .HasColumnType("money")
+                .HasDefaultValueSql("((0))");
+
+            builder.Property(e => e.UnitsInStock)
+                .HasDefaultValueSql("((0))");
+        }
+    }
+}
