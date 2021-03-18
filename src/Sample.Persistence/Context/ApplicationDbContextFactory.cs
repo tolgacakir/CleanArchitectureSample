0701b7fb35ac7cfc3fb14d28e9e8b90f890edcc9
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-18 11:56:11 +0300

    Add ApplicationDbContext Factories for migration

---
+++ src/Sample.Persistence/Context/ApplicationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Persistence.Context
+{
+    public class ApplicationDbContextFactory : DesignTimeDbContextFactoryBase<ApplicationDbContext>
+    {
+        protected override ApplicationDbContext CreateNewInstance(DbContextOptions<ApplicationDbContext> options)
+        {
+            return new ApplicationDbContext(options,null);
+        }
+    }
+}
---
+++ src/Sample.Persistence/Context/DesignTimeDbContextFactoryBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Persistence.Context
+{
+    public abstract class DesignTimeDbContextFactoryBase<TContext> : IDesignTimeDbContextFactory<TContext>
+        where TContext : DbContext
+    {
+        protected abstract TContext CreateNewInstance(DbContextOptions<TContext> options);
+        public TContext CreateDbContext(string[] args)
+        {
+            var basePath = Directory.GetCurrentDirectory() + string.Format("{0}..{0}Sample.WebApi", Path.DirectorySeparatorChar);
+
+            DbContextOptionsBuilder<TContext> builder = new DbContextOptionsBuilder<TContext>();
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return CreateNewInstance(builder.Options);
+        }
+    }
+}
