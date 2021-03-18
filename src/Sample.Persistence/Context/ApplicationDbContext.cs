fc61051cbb8be8fb1116505e038f8a904b3774d3
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-18 11:55:36 +0300

    Add ApplicationDbContext

---
+++ src/Sample.Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample.Application.Common.Interfaces;
+using Sample.Domain.Common;
+using Sample.Domain.Entities;
+using Sample.Persistence.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sample.Persistence.Context
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        private readonly IDateTimeService _dateTimeService;
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options, IDateTimeService dateTimeService) :base(options)
+        {
+            _dateTimeService = dateTimeService;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfiguration()); //alternative: builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity<object>>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = "current user"; //TODO: @ApplicationDbContext, get current user id...
+                        entry.Entity.Created = _dateTimeService.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedBy = "current user";
+                        entry.Entity.LastModified = _dateTimeService.Now;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
