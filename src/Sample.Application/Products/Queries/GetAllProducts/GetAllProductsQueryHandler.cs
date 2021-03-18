75c2eb3d055b365e27ef76acf0e77cdb2a353f26
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-18 11:51:32 +0300

    Add GetAllProduct Query

---
+++ src/Sample.Application/Products/Queries/GetAllProducts/GetAllProductsQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sample.Application.Products.Queries.GetAllProducts
+{
+    public class GetAllProductsQueryHandler
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllProductsQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<GetAllProductsResponse>> GetAllProducts(GetAllProductsRequest request, CancellationToken cancellationToken)
+        {
+            var response = await _context.Products
+                .Select(p => new GetAllProductsResponse
+                {
+                    Name = p.Name,
+                    Price = p.UnitPrice,
+                })
+                .Take(100)
+                .ToListAsync();
+            return response;
+        }
+    }
+}
---
+++ src/Sample.Application/Products/Queries/GetAllProducts/GetAllProductsResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Application.Products.Queries.GetAllProducts
+{
+    public class GetAllProductsResponse
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
