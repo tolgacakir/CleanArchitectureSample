f2467af6c97d0c08132f388b414af7c27fac16e4
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-18 11:50:04 +0300

    Add Category Entity

---
+++ src/Sample.Domain/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using Sample.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Domain.Entities
+{
+    public class Category : Entity<int>
+    {
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+    }
+}
