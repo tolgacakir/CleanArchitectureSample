57f0a2e68c05e2030958e1c8e89a3d75b5a7573a
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:31:31 +0300

    Add LoginAppUserRequestValidator

---
+++ src/Sample.Application/Validation/AppUser/LoginAppUserRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Sample.Application.AppUsers.Queries.LoginAppUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Application.Validation.AppUser
+{
+    public class LoginAppUserRequestValidator : AbstractValidator<LoginAppUserRequest>
+    {
+    }
+}
