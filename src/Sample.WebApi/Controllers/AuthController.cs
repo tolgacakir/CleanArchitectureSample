d38e2f28ac718955850c601d30710ec63063661a
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:57:47 +0300

    Add AuthController

---
+++ src/Sample.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Application.AppUsers.Queries;
+using Sample.Application.AppUsers.Queries.LoginAppUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        IMediator _mediator;
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<LoginAppUserResponse> Login(LoginAppUserRequest request)
+        {
+            return await _mediator.Send(request);
+        }
+    }
+}
