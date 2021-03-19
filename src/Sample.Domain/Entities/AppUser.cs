27b8937876362b7781d9335c38eaa36cef7d8f1b
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:27:09 +0300

    Add AppUser Entity

---
+++ src/Sample.Domain/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Sample.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Domain.Entities
+{
+    public class AppUser : AuditableEntity<int>
+    {
+        public string DisplayName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
