ddee523c3f11da6b0340dd0c209ab0b25a6b2978
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:48:56 +0300

    Add AppUserConfiguration

---
+++ src/Sample.Persistence/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sample.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Persistence.Configurations
+{
+    public class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.Property(p => p.UserName)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            builder.Property(p => p.Email)
+                .IsRequired()
+                .HasMaxLength(40);
+
+            builder.Property(p => p.Password)
+                .IsRequired()
+                .HasMaxLength(20);
+        }
+    }
+}
