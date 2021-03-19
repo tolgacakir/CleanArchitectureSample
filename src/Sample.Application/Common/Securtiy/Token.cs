129850a920be13dc42935189000e09fb7247e0e4
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:46:18 +0300

    Add Token Class

---
+++ src/Sample.Application/Common/Securtiy/Token.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Application.Common.Securtiy
+{
+    public class Token
+    {
+        public string AccessToken { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
