59da6b519e52e5d1a7ec6920b7697e9956a4715e
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:56:00 +0300

    Add CategoriesController

---
+++ src/Sample.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Application.Categories.Commands.CreateCategory;
+using Sample.Application.Categories.Queries.GetAllCategories;
+using Sample.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Sample.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CategoriesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<CreateCategoryResponse> Create([FromBody] CreateCategoryRequest request)
+        {
+            return await _mediator.Send(request);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<GetAllCategoriesResponse>> GetAll()
+        {
+            return await _mediator.Send(new GetAllCategoriesRequest());
+        }
+    }
+}
