e75e2ef1b7d835bb7cd65c062a72a21772563a92
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:43:59 +0300

    Add MappingProfile

---
+++ src/Sample.Application/Mapping/MappingProfile.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Sample.Application.Categories.Commands.CreateCategory;
+using Sample.Application.Categories.Queries.GetAllCategories;
+using Sample.Application.Products.Queries.GetAllProducts;
+using Sample.Application.Products.Queries.GetProductWithFilter;
+using Sample.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Application.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, GetAllProductsResponse>()
+                .ReverseMap();
+            CreateMap<Product, GetAllProductsRequest>()
+                .ReverseMap();
+            
+            CreateMap<Product, GetProductWithFilterRequest>()
+                .ReverseMap();
+            CreateMap<Product, GetProductWithFilterResponse>()
+                .ReverseMap();
+
+            CreateMap<Category, CreateCategoryRequest>()
+                .ReverseMap();
+            CreateMap<Category, CreateCategoryResponse>()
+                .ReverseMap();
+
+            CreateMap<Category, GetAllCategoriesRequest>()
+                .ReverseMap();
+            CreateMap<Category, GetAllCategoriesResponse>()
+                .ReverseMap();
+
+        }
+    }
+}
