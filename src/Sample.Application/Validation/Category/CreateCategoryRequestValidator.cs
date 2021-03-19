3f615f11967624d5b079f94007e7c2d00833285d
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:26:45 +0300

    Add CreateCategoryRequestValidator

---
+++ src/Sample.Application/Validation/Category/CreateCategoryRequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using Sample.Application.Categories.Commands.CreateCategory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Application.Validation.Category
+{
+    public class CreateCategoryRequestValidator : AbstractValidator<CreateCategoryRequest>
+    {
+        public CreateCategoryRequestValidator()
+        {
+            RuleFor(r => r.Name)
+                .NotNull()
+                .WithMessage("Can not null!")
+                .NotEmpty()
+                .WithMessage("Can not empty!")
+                .MaximumLength(15)
+                .WithMessage("Maximum length is 15 charachter");
+                
+        }
+    }
+}
