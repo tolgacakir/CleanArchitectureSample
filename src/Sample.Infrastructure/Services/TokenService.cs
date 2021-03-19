66ac6150333caf1d98e7685ea0bf5847eb60e4ac
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:53:04 +0300

    Add TokenService

---
+++ src/Sample.Infrastructure/Services/TokenService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Sample.Application.Common.Interfaces;
+using Sample.Application.Common.Securtiy;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Infrastructure.Services
+{
+    public class TokenService : ITokenService
+    {
+        public Token CreateAccessToken(int expires)
+        {
+            Token token = new Token();
+
+            IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../Sample.WebApi"))
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            //Security Key
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecurityKey"]));
+
+            //Kimlik
+            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            token.Expiration = DateTime.Now.AddMinutes(expires);
+
+            //JWT Konfigürasyonu
+            JwtSecurityToken securityToken = new JwtSecurityToken(
+                issuer: configuration["JWT:Issuer"],
+                audience: configuration["JWT:Audience"],
+                expires: token.Expiration,
+                notBefore: DateTime.Now, //Token üretildikten ne kadar süre sonra devreye girsin ayarlıyoruz.
+                signingCredentials: signingCredentials,
+                claims: new List<Claim> {
+                    new(ClaimTypes.Role, "misafir"),
+                    new("abc", "xyz"),
+                    new("tarih",DateTime.Now.ToShortTimeString()),
+                    new("site","https://www.google.com"),
+                    new("adres","eskişehir, istiklal"),
+                    new("adisoyad","asdkalsd alsödlaösd"),
+                    new("sirket","Nurettin Nakliyat")
+                });
+
+            //Token oluşturucu
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            token.AccessToken = tokenHandler.WriteToken(securityToken);
+
+            return token;
+        }
+
+        public string CreateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
