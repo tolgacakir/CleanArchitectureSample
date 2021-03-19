50e663eefb977f502b1910e463ceaf3d390d08eb
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:25:32 +0300

    Add ValidationFilter

---
+++ src/Sample.Infrastructure/Filters/ValidationFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Infrastructure.Filters
+{
+    public class ValidationFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                //logla
+                //kullanıcı bilgilendir
+                //mail vs
+
+                var errors = context.ModelState
+                    .Where(e => e.Value.Errors.Count > 0)
+                    .ToDictionary(e => e.Key, e => e.Value.Errors.Select(e => new
+                    {
+                        e.ErrorMessage
+                    })).ToArray();
+
+                context.Result = new BadRequestObjectResult(errors);
+                return;
+            }
+            await next();
+        }
+    }
+}
