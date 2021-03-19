9488ab7d773a313f0d01f840d56b8e35c8aad105
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:44:27 +0300

    Modify name to NotFoundException

---
+++ src/Sample.Application/Common/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Application.Common.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException() : base()
+        {
+        }
+
+        public EntityNotFoundException(string message) : base(message)
+        {
+        }
+
+        public EntityNotFoundException(string name, object key) : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+        }
+    }
+}
