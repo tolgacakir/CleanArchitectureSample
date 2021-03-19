ebf330225cea66ccdf7064c2dc91aa3c6455b3f5
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:48:13 +0300

    Add ServiceCollectionExtensions for Application Layer

---
+++ src/Sample.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Sample.Application.Categories.Queries.GetAllCategories;
+using Sample.Application.Categories.Commands.CreateCategory;
+using Sample.Application.AppUsers.Queries;
+using Sample.Application.AppUsers.Queries.LoginAppUser;
+
+namespace Sample.Application
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+            services.AddTransient<GetAllCategoriesQueryHandler>();
+            services.AddTransient<CreateCategoryCommandHandler>();
+            services.AddTransient<LoginAppUserQueryHandler>();
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
