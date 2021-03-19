ff9598ed3bb26742ca84886aa82d37850da255f7
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:45:55 +0300

    Add IApplicationDbContext

--- src/Sample.Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Sample.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -4,13 +4,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Sample.Application.Common.Interfaces
 {
     public interface IApplicationDbContext
     {
+        DbSet<AppUser> AppUsers { get; set; }
         DbSet<Product> Products { get; set; }
         DbSet<Category> Categories { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
