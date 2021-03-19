2459831284e6893d777834d0d137e80a2f2b17a0
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:46:31 +0300

    Add ITokenService

---
+++ src/Sample.Application/Common/Interfaces/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using Sample.Application.Common.Securtiy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Application.Common.Interfaces
+{
+    public interface ITokenService
+    {
+        Token CreateAccessToken(int expires);
+        string CreateRefreshToken();
+    }
+}
