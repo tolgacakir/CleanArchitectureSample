822b0a230b5fa8182b65826b4292e63921ca03b4
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-19 21:52:51 +0300

    Add CustomExceptionFilter

---
+++ src/Sample.Infrastructure/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Sample.Application.Common.Exceptions;
+
+namespace Sample.Infrastructure.Filters
+{
+    public class CustomExceptionFilter : ExceptionFilterAttribute
+    {
+        //TODO: @CustomExceptionFilter await edilmeyen async metot
+        public async override Task OnExceptionAsync(ExceptionContext context)
+        {
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            if (context.Exception is EntityNotFoundException)
+                statusCode = (int)HttpStatusCode.NotFound;
+
+            context.HttpContext.Response.ContentType = "application/json";
+            context.HttpContext.Response.StatusCode = statusCode;
+            context.Result = new JsonResult(new
+            {
+                error = new[] { context.Exception.Message },
+                source = context.Exception.Source
+            });
+        }
+
+    }
+}
