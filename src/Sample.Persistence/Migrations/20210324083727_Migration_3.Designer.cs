a8184f1a9e87397f66a7e5df0a8064ed5e982fac
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-24 11:59:13 +0300

    Add new migration for AppUser.RefreshToken

--- src/Sample.Persistence/Migrations/20210324083727_Migration_3.Designer.cs
+++ src/Sample.Persistence/Migrations/20210324083727_Migration_3.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sample.Persistence.Context;
 
 namespace Sample.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210324083727_Migration_3")]
+    partial class Migration_3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Sample.Persistence/Migrations/20210324083727_Migration_3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sample.Persistence.Migrations
+{
+    public partial class Migration_3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "AppUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenEndDate",
+                table: "AppUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2021, 3, 24, 11, 37, 27, 40, DateTimeKind.Local).AddTicks(7377));
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2021, 3, 24, 11, 37, 27, 41, DateTimeKind.Local).AddTicks(8286));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "AppUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenEndDate",
+                table: "AppUsers");
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2021, 3, 19, 17, 48, 52, 666, DateTimeKind.Local).AddTicks(7046));
+
+            migrationBuilder.UpdateData(
+                table: "AppUsers",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2021, 3, 19, 17, 48, 52, 667, DateTimeKind.Local).AddTicks(7880));
+        }
+    }
+}
