e5d8940b96539f4b9810987ceb666dbc3b8a0ee9
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-24 12:11:17 +0300

    Update AuthController. Update Route and add LoginWithRefreshToken action

--- src/Sample.WebApi/Controllers/AuthController.cs
+++ src/Sample.WebApi/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Sample.Application.AppUsers.Queries;
 using Sample.Application.AppUsers.Queries.LoginAppUser;
+using Sample.Application.AppUsers.Queries.LoginWithRefreshToken;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace Sample.WebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
@@ -21,6 +22,12 @@ namespace Sample.WebApi.Controllers
         }
         [HttpPost]
         public async Task<LoginAppUserResponse> Login(LoginAppUserRequest request)
+        {
+            return await _mediator.Send(request);
+        }
+
+        [HttpGet]
+        public async Task<LoginWithRefreshTokenResponse> LoginWithRefreshToken(LoginWithRefreshTokenRequest request)
         {
             return await _mediator.Send(request);
         }
