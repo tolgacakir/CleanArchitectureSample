0e7ad9b2b70d4a11f62692090e64977542d368ad
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-24 11:58:33 +0300

    Update AppUser. Add RefreshToken property

--- src/Sample.Domain/Entities/AppUser.cs
+++ src/Sample.Domain/Entities/AppUser.cs
@@ -13,5 +13,7 @@ namespace Sample.Domain.Entities
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenEndDate { get; set; }
     }
 }
