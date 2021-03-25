a03113152983a741d1c656aac6fc5c07a0f2a8b1
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-25 11:07:15 +0300

    Add CreateProductWithCategoryRequestValidator

---
+++ src/Sample.Application/Validation/Product/CreateProductWithCategoryRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Sample.Application.Products.Commands.CreateProductWithCategory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Application.Validation.Product
+{
+    public class CreateProductWithCategoryRequestValidator : AbstractValidator<CreateProductWithCategoryRequest>
+    {
+        public CreateProductWithCategoryRequestValidator()
+        {
+            RuleFor(r => r.CategoryName)
+                .NotNull()
+                .WithMessage("CategoryName can not be null.")
+                .NotEmpty()
+                .WithMessage("CategoryName can not be empty")
+                .MinimumLength(2)
+                .WithMessage("Minimum length is 2 for CategoryName");
+        }
+    }
+}
