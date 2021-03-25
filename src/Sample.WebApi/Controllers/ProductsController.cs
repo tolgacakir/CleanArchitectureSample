252d30571b8982ff028b5a6d8bb5ce1b5640b442
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-25 11:09:08 +0300

    Add ProductsController

---
+++ src/Sample.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Application.Products.Commands.CreateProductWithCategory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<CreateProductWithCategoryResponse> CreateProductWithCategory([FromBody] CreateProductWithCategoryRequest request)
+        {
+            return await _mediator.Send(request);
+        }
+    }
+}
