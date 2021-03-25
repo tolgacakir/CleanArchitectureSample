ad88ea9cc9f3130024f17407bde97a836f810c3d
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-25 11:08:49 +0300

    Delete unrequired comment

--- src/Sample.WebApi/Controllers/CategoriesController.cs
+++ src/Sample.WebApi/Controllers/CategoriesController.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Sample.WebApi.Controllers
 {
