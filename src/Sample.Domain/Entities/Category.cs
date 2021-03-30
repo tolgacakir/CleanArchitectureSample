af5b26351981db43e5b875985c2f3197897342c6
Tolga Çakır <cakrtolga13@gmail.com>
2021-03-30 15:39:30 +0300

    Make ISoftable the Category entity

--- src/Sample.Domain/Entities/Category.cs
+++ src/Sample.Domain/Entities/Category.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 
 namespace Sample.Domain.Entities
 {
-    public class Category : Entity
+    public class Category : Entity, ISoftDelete
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public ICollection<Product> Products { get; set; }
+        public bool IsDeleted { get; set; }
+
         public Category()
         {
             Products = new HashSet<Product>();
